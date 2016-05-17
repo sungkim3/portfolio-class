13fe797bafc4696f6be642b68e1b4b15c4baf1dd
sungkim3 <sng.kim103@gmail.com>
2016-05-17 16:01:34 -0700

    includes IIFE functionality and functional programming, cleans up some code

--- js/app.js
+++ js/app.js
@@ -1,79 +1,77 @@
-function Project (obj) {
-  this.category = obj.category;
-  this.title = obj.title;
-  this.author = obj.author;
-  this.authorUrl = obj.authorUrl;
-  this.publishDate = obj.publishDate;
-  this.body = obj.body;
-  this.imagePlaceholder = obj.imagePlaceholder;
-  // for (keys in opts) {
-  //   this[keys] = opts[keys];
-  // }
-}
-
-Project.all = [];
-
-Project.prototype.toHtml = function(scriptTemplateId) {
-  // var $source = scriptTemplateId.html();
-  var template = Handlebars.compile((scriptTemplateId).html());
-  return template(this);
-};
-
-Project.loadAll = function(dataWePassIn) {
-  dataWePassIn.sort(function(a,b) {
-    return (new Date(b.publishDate)) - (new Date(a.publishDate));
-  });
-  dataWePassIn.forEach(function(ele) {
-    Project.all.push(new Project(ele));
-  });
-};
-
-Project.fetchAll = function() {
-  if(localStorage.stringedProjects) {
-    $.ajax({
-      url: '/data/projectData.json',
-      success: function(data, message, xhr) {
-        Project.all = [];
-        var eTag = xhr.getResponseHeader('eTag');
-        if (eTag !== JSON.parse(localStorage.getItem('stringedTag'))) {
-          console.log(eTag + ' is different from stored ' + JSON.parse(localStorage.getItem('stringedTag')));
-          Project.getData('/data/projectData.json');
-        } else {
-          console.log('eTags are the same');
-          Project.loadAll(JSON.parse(localStorage.getItem('stringedProjects')));
-          Project.appendProjects(Project.all);
-          projectView.initIndexPage();
-        }
-      }
-    });
-
-  } else {
-    console.log('I do not have local storage');
-    $.ajax({
-      url: '/data/projectData.json',
-      success: function(data, message, xhr) {
-        Project.getData('/data/projectData.json');
-        var eTag = xhr.getResponseHeader('eTag');
-        console.log(eTag);
-        localStorage.setItem('stringedTag', JSON.stringify(eTag));
-      }
-    });
-
+(function (module) {
+  function Project (opts) {
+    for (keys in opts) {
+      this[keys] = opts[keys];
+    }
   }
-};
 
-Project.appendProjects = function(projectArray) {
-  projectArray.forEach(function(projectItem){
-    $('#projects').append(projectItem.toHtml($('#project-template')));
-  });
-};
+  Project.all = [];
 
-Project.getData = function(filePath) {
-  $.getJSON(filePath, function(data) {
+  Project.prototype.toHtml = function(scriptTemplateId) {
+    var template = Handlebars.compile((scriptTemplateId).html());
+    return template(this);
+  };
+
+  Project.loadAll = function(dataWePassIn) {
+    dataWePassIn.sort(function(a,b) {
+      return (new Date(b.publishDate)) - (new Date(a.publishDate));
+    });
+    dataWePassIn.forEach(function(ele) {
+      Project.all.push(new Project(ele));
+    });
+  };
+
+  Project.fetchAll = function(next) {
+    if(localStorage.stringedProjects) {
+      $.ajax({
+        type: 'HEAD',
+        url: '/data/projectData.json',
+        success: function(data, message, xhr) {
+          var eTag = xhr.getResponseHeader('eTag');
+          if (!localStorage.eTag || eTag !== localStorage.eTag) {
+            localStorage.eTag = eTag;
+            Project.getData(next);
+          } else {
+            Project.loadAll(JSON.parse(localStorage.stringedProjects));
+            next();
+          }
+        }
+      });
+    } else {
+      console.log('I do not have local storage');
+      Project.getData(next);
+    }
+  };
+
+  Project.getData = function(next) {
     console.log('I am retrieving data');
-    Project.loadAll(data);
-    localStorage.setItem('stringedProjects', JSON.stringify(Project.all));
-    Project.appendProjects(Project.all);
-    projectView.initIndexPage();
-  });
-};
+    $.getJSON('/data/projectData.json', function(data) {
+      Project.loadAll(data);
+      localStorage.stringedProjects = JSON.stringify(Project.all);
+      next();
+    });
+  };
+//This was for the portfolio requirements in which we were expected to demonstrate
+//our ability to use a .map and .reduce method. The purpose of this function is
+//to collect all the titles (non redundant) into a new array.
+  Project.allTitles = function() {
+    return Project.all.map(function(project) {
+      return project.title;
+    })
+    .reduce(function(titles, title) {
+      if(titles.indexOf(title) === -1) {
+        titles.push(title);
+      }
+      console.log(titles);
+      return titles;
+    }, []);
+  };
+
+  Project.numWordsArticle = function() {
+    return Project.all.forEach(function(project) {
+      project.numWords = project.body.match(/\b\w+/g).length;
+    });
+  };
+
+  module.Project = Project;
+})(window);
